4f78fb5d76b83dc5d686ef8a86e0eb6d293cb5e7
Eshwar Lal <laleshwar35@example.com>
2026-07-30 20:07:33 +0500

    Stage 4: update and delete endpoints

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from pydantic import BaseModel
 app = FastAPI()
 class TaskCreate(BaseModel):
     title: str
-
+class TaskUpdate(BaseModel):
+    title: str
+    done: bool
 tasks = [
     {
         "id": 1,
@@ -66,3 +68,36 @@ def create_task(task: TaskCreate):
     tasks.append(new_task)
 
     return new_task
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: TaskUpdate):
+
+    for task in tasks:
+
+        if task["id"] == task_id:
+
+            task["title"] = updated_task.title
+            task["done"] = updated_task.done
+
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+
+    for task in tasks:
+
+        if task["id"] == task_id:
+
+            tasks.remove(task)
+
+            return {
+                "message": f"Task {task_id} deleted successfully"
+            }
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
